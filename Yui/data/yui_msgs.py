0cf324959bd478342aef437ba9e202d0ee84aef4
Yürüyencesed <91276875+Ruhsuzbey@users.noreply.github.com>
2022-07-16 11:25:58 +0300

    Update yui_msgs.py

--- Yui/data/yui_msgs.py
+++ Yui/data/yui_msgs.py
@@ -1,54 +1,44 @@
 # Copyright (c) 2021 Itz-fork
 
 Emergency_Msgs = [
-    "I'm not feeling well. I think i should go and sleep now.",
-    "I have some work to do. Bye!",
-    "I'm very tired. I need to take a nap.",
-    "Ooops!, I think my owner forgot to pay my bill!",
-    "Oh, I think something happened to me!",
-    "Nice to meet you but I have to go now, Bye!"
-]
+    "Kendimi iyi hissetmiyorum. Sanırım şimdi gidip uyumalıyım.",
+     "Yapacak işlerim var. Görüşürüz!",
+     "Çok yorgunum. Biraz kestirmem gerekiyor.",
+     "Hata!, sanırım sahibim faturamı ödemeyi unuttu!",
+     "Ah, sanırım bana bir şey oldu!",
+     "Tanıştığıma memnun oldum ama şimdi gitmem gerekiyor, hoşçakal!"]
 
 Photo_Reesponse = [
-    "Woah, that's a nice picture",
-    "Hmmm... This one looks prettry good!",
-    "What's this?",
-    "IMHO This is the worst image I've ever seen in my life",
-    "Pathetic...",
-    "Ok? I guess",
-    "I don't like photos :(",
-    "This image is weird",
-    "I don't know what to do with this image",
-    "Hey, can we please chat only using text messages? because I can't understand photos correctly :("
-]
+"Vay, bu güzel bir resim",
+     "Hmmm... Bu oldukça iyi görünüyor!",
+     "Bu nedir?",
+     "IMHO Bu hayatımda gördüğüm en kötü görüntü",
+     "Acınası...",
+     "Tamam sanırım",
+     "Fotoğrafları sevmiyorum :(",
+     "Bu görüntü tuhaf",
+     "Bu görüntüyle ne yapacağımı bilmiyorum",
+     "Hey, lütfen sadece kısa mesaj kullanarak sohbet edebilir miyiz? çünkü fotoğrafları doğru anlayamıyorum :("]
 
 Sticker_Response = [
-    "Aww, nice sticker",
-    "I like this sticker ngl.",
-    "Who made this?",
-    "This sticker looks terrible.",
-    "Uhmmm... So what's up with this sticker?",
-    "Ok, cool",
-    "I love these types of stickers",
-    "Can we please chat only using text messages? because I can't understand stickers correctly :("
-]
+"Ah, güzel çıkartma",
+     "Bu çıkartmayı beğendim.",
+     "Bunu kim yaptı?",
+     "Bu çıkartma korkunç görünüyor.",
+     "Uhmmm... Peki bu çıkartmanın nesi var?",
+     "Tamam iyi",
+     "Bu tür çıkartmaları seviyorum",
+     "Lütfen sadece kısa mesaj kullanarak sohbet edebilir miyiz? çünkü çıkartmaları doğru anlayamıyorum :("]
 
 Video_and_gif_Response = [
-    "What is this?",
-    "I don't wanna waste my data to download this file",
-    "What do you want?",
-    "Can you elborate?",
-    "I don't watch videos",
-    "Is this clip from a movie?",
-    "uWu, this looks nice!",
-    "I can't understand videos correctly :( . Let's chat only using text messages"
-]
+"Bu nedir?",
+     "Bu dosyayı indirmek için verilerimi boşa harcamak istemiyorum",
+     "Ne istiyorsun?",
+     "Açıklayabilir misin?",
+     "Video izlemiyorum",
+     "Bu klip bir filmden mi?",
+     "uWu, bu güzel görünüyor!",
+     "Videoları doğru anlayamıyorum :( . Sadece kısa mesaj kullanarak sohbet edelim"]
 
 Document_Response = [
-    "I don't wanna waste my data to download this file",
-    "Sorry, what?",
-    "What is this file anyway?",
-    "Looks like this a virus '-'",
-    "I don't like this",
-    "Why did you send me this? Can't we use text messages to chat?"
-]
+    "Bu dosyayı indirmek için verilerimi boşa harcamak istemiyorum",]
